94f077b3ab89292bdd3a0eb0a4606cf66041630d
Kasper Botker <kasper.botker@sintef.no>
2020-12-08 16:08:27 +0100

    added read example

---
+++ examples/read_archive_sine.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(0,"..")
+
+from autoactive.autoactive.archivereader import ArchiveReader
+
+from pathlib import Path
+
+fname = Path(r"C:\Users\kasperb\Documents\Projects\AutoActive\Kode\autoactive-python\examples\DOESITWORK.aaz")
+ar = ArchiveReader(fname)
+ids = ar.list_ids()
+s = ar.open_session(ids[0])
+
+print(s.python_data.trig.sine.data)
