d892fa72215c111ed92c19c841f5cda238cf0f05
Kasper Botker <kasper.botker@sintef.no>
2021-01-13 16:16:30 +0100

    documented code

--- autoactive/autoactive/source.py
+++ autoactive/autoactive/source.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Source(Fileobject):
+    """ Object representing the source """
 
     def __init__(self):
         super().__init__()
@@ -12,15 +13,24 @@ class Source(Fileobject):
         self.meta.version: int = 1
         self.user.language: str = "PYTHON"
 
-    def from_serializable(self, archive_reader):
+    def from_serializable(self, _):
         return self
 
     @classmethod
     def from_dict(cls, dict_, archive_reader):
+        """ Class constructor used when object is
+            constructed from file
+
+        :arg
+            dict_ (dict): Metadata used for creating the Source object
+
+            archive_reader (ArchiveReader): Object used for reading data
+            from aaz file
+
+        :returns
+            obj (Session): Object representing the source
+        """
         obj = Source.__new__(cls)
         super().__init__(obj)
         obj.to_natives(dict_, archive_reader)
         return obj
-
-
-
