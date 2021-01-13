87030cfd32598ad27462ea2ead38efa856ac1f8f
Kasper Botker <kasper.botker@sintef.no>
2021-01-13 10:19:09 +0100

    refractored code

--- examples/read_archive_sine.py
+++ examples/read_archive_sine.py
@@ -1,13 +1,14 @@
 import sys
-sys.path.insert(0,"..")
+
+sys.path.insert(0, "..")
 
 from autoactive.autoactive.archivereader import ArchiveReader
 
 from pathlib import Path
 
-fname = Path(r"C:\Users\kasperb\Documents\Projects\AutoActive\Kode\autoactive-python\examples\DOESITWORK.aaz")
+fname = Path(
+    r"C:\Users\kasperb\Documents\Projects\AutoActive\Kode\autoactive-python\examples\DOESITWORK.aaz"
+)
 ar = ArchiveReader(fname)
 ids = ar.list_ids()
 s = ar.open_session(ids[0])
-
-print(s.python_data.trig.sine.data)
