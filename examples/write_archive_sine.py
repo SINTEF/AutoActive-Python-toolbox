bf40352dc42c46a2cc7096e5f04024fa5f953b3f
Kasper Botker <kasper.botker@sintef.no>
2021-01-14 14:47:22 +0100

    documented code

--- examples/write_archive_sine.py
+++ examples/write_archive_sine.py
@@ -1,3 +1,6 @@
+""" Script for creating a session with 1d sensor data
+    and saving the session to a aaz file """
+
 import sys
 sys.path.insert(0,"..")
 
@@ -10,36 +13,70 @@ from autoactive.autoactive.archivewriter import ArchiveWriter
 import __main__
 from pathlib import Path
 import numpy as np
+from argparse import ArgumentParser
 
-so = Source()
-this_path = Path(__main__.__file__)
-so.add_content_from_file_to_archive(this_path)
+def save_session(session, fname):
+    """ Saves session to file
 
-time = np.array(range(0,100000000,100000)).astype("int64")
-sine = np.sin(2*3.14*time/1000000).astype("float64")
-cosi = np.cos(2*3.14*time/1000000).astype("float64")
-time_off = time + 1000000
+    :arg
+        session (Session): session object to save to file
 
-table = Datatable()
-table.time = time
-table.time.unit = "Epocms"
-table.sine = sine
-table.cosi = cosi
+        fname (Path): Path to where to save the session object
+    """
 
-table_off = Datatable()
-table_off.time = time_off
-table_off.time.unit = "Epocms"
-table_off.sine = sine
-table_off.cosi = cosi
+    aw = ArchiveWriter(fname)
+    aw.save_session(session)
+    aw.close()
 
-session = Session(session_name= "Session")
-session.python_data = Folder()
-session.python_data.trig = table
-session.python_data.trig_off = table_off
-session.source = so
+def create_sample_datatable(offset = 0):
+    """ Adds sample data to datatable
 
-write_to = Path.joinpath(this_path.parent, "DOESITWORK.aaz")
-aw = ArchiveWriter(write_to)
-aw.save_session(session)
-aw.close()
+    :arg
+        offset (int): Offset of timeline
 
+    :returns
+        table (Datatable): dataobject storing 1d sensor data
+    """
+
+    time = np.array(range(0, 100000000, 100000)).astype("int64") + offset
+    sine = np.sin(2 * 3.14 * time / 1000000).astype("float64")
+    cosi = np.cos(2 * 3.14 * time / 1000000).astype("float64")
+
+    table = Datatable()
+    table.time = time
+    table.time.unit = "Epocms"
+    table.sine = sine
+    table.cosi = cosi
+    return table
+
+def main(fname):
+    """ Creates a session and saves it to an aaz file
+
+    :arg:
+        fname (Path): Path to where to save the session
+    """
+    so = Source()
+    this_path = Path(__main__.__file__)
+    so.add_content_from_file_to_archive(this_path)
+
+    table = create_sample_datatable()
+    table_off = create_sample_datatable(1000000)
+
+    session = Session(session_name= "Session")
+    session.python_data = Folder()
+    session.python_data.trig = table
+    session.python_data.trig_off = table_off
+    session.source = so
+
+    save_session(session, fname)
+
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("fname")
+    args = parser.parse_args()
+
+    fname = Path(args.fname)
+    assert fname.name.split(".")[-1] == "aaz", "The file must be an aaz file"
+    main(fname)
