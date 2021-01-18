a407f61a70a0b2d97de9ac24c18535729fd86659
Kasper Botker <kasper.botker@sintef.no>
2021-01-18 10:33:10 +0100

    refractored code

--- autoactive/datastructures/video.py
+++ autoactive/datastructures/video.py
@@ -9,23 +9,8 @@ class Video(Fileobject):
         super().__init__()
         self.meta.type = "no.sintef.video"
         self.meta.version = 1
-
-    def to_serializable(self, **kwargs):
-
-        """ Method which transforms the video object to a
-        serializable object
-
-        :arg
-            **kwargs (dict): dictionary containing the archiveWriter
-            uuid and the parent key
-
-        :returns
-            video_obj_json (dict): representing the video object as a dictionary
-
-        """
-
         self.meta.time_scale = 1
         self.meta.start_time = 0
         self.meta.is_world_clock = False
-        video_obj_json = super().to_serializable(**kwargs)
-        return video_obj_json
+
+
