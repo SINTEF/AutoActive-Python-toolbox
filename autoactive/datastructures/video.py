0cfe35dc97217ace110a46badc4d4c0bb3b753dd
Kasper Botker <kasper.botker@sintef.no>
2021-01-18 11:17:06 +0100

    add func for reading archives with videos

--- autoactive/datastructures/video.py
+++ autoactive/datastructures/video.py
@@ -1,4 +1,6 @@
 from autoactive.archive.fileobject import Fileobject
+from autoactive.datastructures.meta import Meta
+from autoactive.datastructures.user import User
 
 
 class Video(Fileobject):
@@ -13,4 +15,31 @@ class Video(Fileobject):
         self.meta.start_time = 0
         self.meta.is_world_clock = False
 
+    def from_serializable(self, _):
+        """ Overwrites the implementation in Dataobject,
+            should not read video into memory when data
+            is read from aaz file, as they may be very large
+            and not always necessary
+        """
+        return self
 
+    @classmethod
+    def from_dict(cls, dict_, archive_reader):
+        """ Class constructor used when object is
+            constructed from file
+
+        :arg
+            dict_ (dict): Metadata used for creating the Video object
+
+            archive_reader (ArchiveReader): Object used for reading data
+            from aaz file
+
+        :returns
+            obj (Session): Pythonic representation of a session
+        """
+
+        obj = Video.__new__(cls)
+        obj._meta = Meta()
+        obj._user = User()
+        obj.to_natives(dict_, archive_reader)
+        return obj
