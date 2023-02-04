c8c4a428bb350a77a10a97cb44467e13846da7c9
oyvindgloersen <101183384+oyvindgloersen@users.noreply.github.com>
2023-02-04 17:56:27 +0100

    add annotations-example to "write_annotations_sine.py". Remove annotation-specific examples.

--- examples/write_archive_sine.py
+++ examples/write_archive_sine.py
@@ -10,6 +10,7 @@ from autoactive.autoactive.datatable import Datatable
 from autoactive.autoactive.session import Session
 from autoactive.autoactive.folder import Folder
 from autoactive.autoactive.archivewriter import ArchiveWriter
+from autoactive.autoactive.annotation import Annotation
 
 from pathlib import Path
 import numpy as np
@@ -39,15 +40,34 @@ def create_sample_datatable(offset=0):
     """
 
     time = np.array(range(0, 100000000, 100000)).astype("int64") + offset
-    sine = np.sin(2 * 3.14 * time / 1000000).astype("float64")
-    cosi = np.cos(2 * 3.14 * time / 1000000).astype("float64")
+    sine = np.sin(2 * np.pi * time / 1000000).astype("float64")
 
     table = Datatable()
     table.time = time
     table.time.unit = "Epocms"
     table.sine = sine
-    table.cosi = cosi
     return table
+
+
+def annotate_sine(table):
+    period = 1000000
+    idx_zeros = np.where(np.remainder(2 * table.as_dataframe.time.values, period) == 0)
+    zeros = table.as_dataframe.time.loc[idx_zeros]
+    peaks = zeros[0:-1:2] + period / 4
+    valleys = peaks + period / 2
+    annotations = Annotation()
+    for zero in zeros:
+        annotations.addAnnotation(zero, 1)
+    for peak in peaks:
+        annotations.addAnnotation(peak, 2)
+    for valley in valleys:
+        annotations.addAnnotation(valley, 3)
+
+    annotations.setAnnotationInfo(1, "zero", "zero", "This is a zero")
+    annotations.setAnnotationInfo(2, "peak", "peak", "This is a maximum")
+    annotations.setAnnotationInfo(3, "valley", "valley", "This is a minimum")
+
+    return annotations
 
 
 def main(fname):
@@ -61,12 +81,12 @@ def main(fname):
     so.add_content_from_file_to_archive(this_path)
 
     table = create_sample_datatable()
-    table_off = create_sample_datatable(1000000)
+    annotations = annotate_sine(table)
 
     session = Session(session_name="Session")
     session.python_data = Folder()
     session.python_data.trig = table
-    session.python_data.trig_off = table_off
+    session.annotations = annotations
     session.source = so
 
     save_session(session, fname)
